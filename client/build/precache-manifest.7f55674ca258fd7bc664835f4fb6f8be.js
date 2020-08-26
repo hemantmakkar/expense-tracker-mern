5b9449d6806094cded7112121c74e71cd6ff6dba
Hemant Makkar <gautam.makkar2001@gmail.com>
2020-08-26 19:52:01 +0530

    Initial Commit

---
+++ client/build/precache-manifest.7f55674ca258fd7bc664835f4fb6f8be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56c28eed6aa41a5f72881c90ca9e0960",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2dc14ce3787a93aeb7ee",
+    "url": "/static/css/main.ee45eb7d.chunk.css"
+  },
+  {
+    "revision": "cd6d70e514ef5d28a595",
+    "url": "/static/js/2.f92b2550.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f92b2550.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dc14ce3787a93aeb7ee",
+    "url": "/static/js/main.64de9c88.chunk.js"
+  },
+  {
+    "revision": "fd1e764c12408bbbbfc1",
+    "url": "/static/js/runtime-main.f859b388.js"
+  }
+]);
---
+++ client/src/components/Transaction.js
@@ -0,0 +1,20 @@
+import React, { useContext } from "react";
+import { GlobalContext } from '../contexts/GlobalState';
+
+export const Transaction = ({ transaction }) => {
+  const sign = transaction.amount < 0 ? "-" : "+";
+
+  const { deleteTransaction } = useContext(GlobalContext);
+
+  return (
+    <>
+      <li className={sign === "-" ? "minus" : "plus"}>
+        {transaction.text}{" "}
+        <span>
+          {sign}${Math.abs(transaction.amount)}
+        </span>
+        <button className="delete-btn" onClick={() => deleteTransaction(transaction._id)}>x</button>
+      </li>
+    </>
+  );
+};
